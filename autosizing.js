55194e1ce75734458a54cce61245d04f2face7fe
Philip Rogers <pdr@chromium.org>
2015-11-07 21:08:48 -0800

    Initial commit with tests passing using the inspector

---
+++ autosizing.js
@@ -0,0 +1,59 @@
+'use strict';
+
+// Determine if Chrome's text autosizer (aka font boosting) is running.
+//
+// This code closely follows the algorithm used:
+// https://code.google.com/p/chromium/codesearch#chromium/src/third_party/WebKit/Source/core/layout/TextAutosizer.cpp&l=554
+var chromeAutosizingText = function() {
+    // Determine if a meta viewport is set at all.
+    // If Chrome ships the CSS Device Adaption spec, this will need to be modified to check for the
+    // @viewport CSS rule (see: https://www.chromestatus.com/feature/4737164243894272).
+    var metaViewportValue = false;
+    var metaTags = document.getElementsByTagName("meta");
+    for (var m = 0; m < metaTags.length; m++) {
+        var metaTag = metaTags[m];
+        if (metaTag.getAttribute("name") == "viewport" && metaTag.getAttribute("content")) {
+            metaViewportValue = true;
+            break;
+        }
+    }
+
+    var deviceScaleAdjustment;
+    if (metaViewportValue) {
+        // A meta viewport disables the device scale adjustment.
+        deviceScaleAdjustment = 1;
+    } else {
+        // An additional device scale adjustment is applied based on Android's smallestScreenWidthDp
+        // setting [1] which is then mapped through a device scale adjustment formula [2] which
+        // linearly interpolates in the range (1.05, 1.3).
+        //
+        // For now we just use the midpoint because this is not a huge difference: 1.175.
+        //
+        // TODO(pdr): We may be able to compute smallestScreenWidthDp exactly using
+        //            window.devicePixelRatio and window.outerWidth.
+        //
+        // [1] http://developer.android.com/reference/android/content/res/Configuration.html#smallestScreenWidthDp
+        // [2] https://code.google.com/p/chromium/codesearch#chromium/src/chrome/browser/chrome_content_browser_client.cc&l=592
+        deviceScaleAdjustment = 1.175;
+    }
+
+    // This value is set via the accessibility preferences and cannot be sniffed directly.
+    // We assume the default value of 100%.
+    var accessibilityFontScaleFactor = 1;
+
+    // We are unable to determine the true screen size from within cross-domain iframes and the
+    // following will throw a SecurityError if we try.
+    var innerWidth = window.top.innerWidth;
+    var outerWidth = window.top.outerWidth;
+
+    var debugText = "";
+    debugText += ("deviceScaleAdjustment(" + deviceScaleAdjustment + ") ");
+    debugText += ("innerWidth(" + innerWidth + ") ");
+    debugText += ("outerWidth(" + outerWidth + ") ");
+    debugText += ("result(" + (deviceScaleAdjustment * accessibilityFontScaleFactor * innerWidth / outerWidth) + ") ");
+    setTimeout(function() {
+        window.alert(debugText);
+    }, 200);
+
+    return deviceScaleAdjustment * accessibilityFontScaleFactor * innerWidth / outerWidth > 1;
+}
